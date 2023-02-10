0bfc6ec6091127a6d664af5a6bdd381f74377b77
Maciej Laskowski <mlaskowski11@gmail.com>
2023-02-10 20:27:45 +0100

    1st example of hoisting added

---
+++ app.js
@@ -0,0 +1,10 @@
+let a = 5;
+
+console.log(a);
+
+// in this excample we get 5 in console log - as expected
+// what really happens while hoisting is:
+// let a;
+// a = 5;
+// console.log(a);
+// so yes, we get 5
