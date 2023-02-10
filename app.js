a051bdcc89ca74e4ab4133ab97318f7765f65110
Maciej Laskowski <mlaskowski11@gmail.com>
2023-02-10 20:35:59 +0100

    what is important - let and consts are not hoisted

--- app.js
+++ app.js
@@ -1,4 +1,4 @@
-let a = 5;
+var a = 5;
 
 console.log(a);
 
@@ -8,3 +8,17 @@ console.log(a);
 // a = 5;
 // console.log(a);
 // so yes, we get 5
+
+console.log(b);
+
+var b = 3;
+
+console.log(b);
+
+// what happens here, is we get undefined as value of console log
+// this is because due to hoisting declaration of let b is move to the top of the code
+// then console log and initialization remains in the same place
+// so console log happens before value 3 is assigned to the variable
+// let b;
+// console.log(b) ---> undefined
+// b = 3;
